a4750f779d16dfd652370aaa3979273b20532732
alaabn <bennasralaa@gmail.com>
2023-09-09 11:09:13 +0100

    initializing the project

---
+++ src/main/java/com/springbootapi/gestionpatient/GestionpatientApplication.java
@@ -0,0 +1,13 @@
+package com.springbootapi.gestionpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionpatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionpatientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootapi/gestionpatient/GestionpatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapi.gestionpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionpatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
