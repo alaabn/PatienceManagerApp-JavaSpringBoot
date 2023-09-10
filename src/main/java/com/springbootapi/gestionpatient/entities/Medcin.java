a58e20eb0b754b86aa644c8c3d5608a6e15dcc26
alaabn <bennasralaa@gmail.com>
2023-09-10 19:09:50 +0100

    Fix Attribute accessibility for Medcin Entity

--- src/main/java/com/springbootapi/gestionpatient/entities/Medcin.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/Medcin.java
@@ -1,8 +1,8 @@
 package com.springbootapi.gestionpatient.entities;
 
 public class Medcin {
-    public int id;
-    public String nom;
-    public String email;
-    public String specialite;
+    private int id;
+    private String nom;
+    private String email;
+    private String specialite;
 }
