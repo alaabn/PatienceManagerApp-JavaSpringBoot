c5d5a2df373c011ccbcaf12e0f752d43bdbaab14
alaabn <bennasralaa@gmail.com>
2023-09-10 19:12:43 +0100

    Add Lombok and Entity annotations

--- src/main/java/com/springbootapi/gestionpatient/entities/Medcin.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/Medcin.java
@@ -1,5 +1,14 @@
 package com.springbootapi.gestionpatient.entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Medcin {
     private int id;
     private String nom;
