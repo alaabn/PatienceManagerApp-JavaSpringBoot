6315a655dc7377632045d9ac2fc4d5b2a22557f4
Enis-Hachicha <hachicha.enis@gmail.com>
2023-09-13 20:09:27 +0100

    fix relational mapping of consultation entity

--- src/main/java/com/springbootapi/gestionpatient/entities/Consultation.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/Consultation.java
@@ -3,12 +3,10 @@ package com.springbootapi.gestionpatient.entities;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,7 +23,6 @@ public class Consultation {
     private String rapportConsultation;
     private Double prixConsultation;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "medecin_id")
-    private Medecin medecin;
+    @OneToOne(mappedBy = "consultation")
+    private RendezVous rendezVous;
 }
