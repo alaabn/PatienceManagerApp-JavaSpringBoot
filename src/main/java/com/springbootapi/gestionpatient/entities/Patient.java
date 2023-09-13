6882a8e2aca2ca5cb8691a5e96d059f02cbbdca2
Enis-Hachicha <hachicha.enis@gmail.com>
2023-09-13 20:08:19 +0100

    add ID annotation, OneToMany relation w RDV entity

--- src/main/java/com/springbootapi/gestionpatient/entities/Patient.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/Patient.java
@@ -1,7 +1,14 @@
 package com.springbootapi.gestionpatient.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +18,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
     private String email;
+
+    @OneToMany(mappedBy = "Patient", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RendezVous> reviews = new ArrayList<RendezVous>();
 }
