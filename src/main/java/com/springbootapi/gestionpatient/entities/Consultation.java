41bb453f0899fba7f313e5aeb0a80df54e09173a
alaabn <bennasralaa@gmail.com>
2023-09-13 19:57:48 +0100

    Add ManyToOne relation to Consultation Entity

--- src/main/java/com/springbootapi/gestionpatient/entities/Consultation.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/Consultation.java
@@ -3,6 +3,12 @@ package com.springbootapi.gestionpatient.entities;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +18,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Consultation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date dateConsultation;
     private String rapportConsultation;
     private Double prixConsultation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medecin_id")
+    private Medecin medecin;
 }
