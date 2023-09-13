4aa4ec3337354470e0bd33ae483eceb8b458525d
alaabn <bennasralaa@gmail.com>
2023-09-13 19:58:43 +0100

    Add OneToMany relation to Medecin Entity

---
+++ src/main/java/com/springbootapi/gestionpatient/entities/Medecin.java
@@ -0,0 +1,30 @@
+package com.springbootapi.gestionpatient.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Medecin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    private String email;
+    private String specialite;
+
+    @OneToMany(mappedBy = "medecin", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Consultation> consultations = new ArrayList<Consultation>();
+}
