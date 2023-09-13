f13b53d15041e82686455fe272c0181da17c37ca
Enis-Hachicha <hachicha.enis@gmail.com>
2023-09-13 20:08:54 +0100

    add relational mappings and ID annotations

--- src/main/java/com/springbootapi/gestionpatient/entities/RendezVous.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/RendezVous.java
@@ -1,9 +1,15 @@
 package com.springbootapi.gestionpatient.entities;
 
-
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +19,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RendezVous {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date dateRDV;
     private Date heureRDV;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medecin_id")
+    private Medecin medecin;
+
+    @OneToOne
+    private Consultation consultation;
 }
