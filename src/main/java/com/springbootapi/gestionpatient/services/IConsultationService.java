9f9ac95290f03169aa3f11ed86561ef0d10e6800
alaabn <bennasralaa@gmail.com>
2023-09-18 22:17:19 +0100

    Add Consultation Interface and Service Impl.

---
+++ src/main/java/com/springbootapi/gestionpatient/services/IConsultationService.java
@@ -0,0 +1,20 @@
+package com.springbootapi.gestionpatient.services;
+
+import java.util.Date;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.springbootapi.gestionpatient.entities.Consultation;
+
+public interface IConsultationService {
+    public void addOrUpdateConsultation(Consultation c);
+
+    public Page<Consultation> getAllConsultationPaginated(Date date, Pageable p);
+
+    public Consultation getConsultationById(Long id);
+
+    public void deleteConsultationById(Long id);
+
+    public Page<Consultation> filterConsultationByDate(Date date, Pageable p);
+}
