a5b27d8a2744d6bbc35882e1636ae18d5a84dde7
alaabn <bennasralaa@gmail.com>
2023-09-18 22:14:15 +0100

    Fix id Type and OneToMany Relationship

--- src/main/java/com/springbootapi/gestionpatient/entities/Medecin.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/Medecin.java
@@ -5,26 +5,31 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Medecin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String nom;
     private String email;
     private String specialite;
 
-    @OneToMany(mappedBy = "medecin", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "medecin", targetEntity = RendezVous.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<RendezVous> rendezVous = new ArrayList<RendezVous>();
 }
