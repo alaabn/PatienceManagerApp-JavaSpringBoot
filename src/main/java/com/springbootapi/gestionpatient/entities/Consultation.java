6bc425cc62b3317c379931ffab431c85f7d560c6
alaabn <bennasralaa@gmail.com>
2023-09-18 22:13:27 +0100

    Fix Date Type Format and Add OneToOne Relation

--- src/main/java/com/springbootapi/gestionpatient/entities/Consultation.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/Consultation.java
@@ -2,27 +2,38 @@ package com.springbootapi.gestionpatient.entities;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Consultation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateConsultation;
     private String rapportConsultation;
     private Double prixConsultation;
 
-    @OneToOne(mappedBy = "consultation")
+    @OneToOne(targetEntity = RendezVous.class, fetch = FetchType.LAZY, orphanRemoval = true)
     private RendezVous rendezVous;
 }
