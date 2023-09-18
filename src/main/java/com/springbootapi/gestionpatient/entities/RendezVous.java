d70afbd09042056843802eaa6d39a34f8e2be464
alaabn <bennasralaa@gmail.com>
2023-09-18 22:15:53 +0100

    Fix Date Type and Relational Mapping

--- src/main/java/com/springbootapi/gestionpatient/entities/RendezVous.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/RendezVous.java
@@ -1,38 +1,49 @@
 package com.springbootapi.gestionpatient.entities;
 
+import java.time.LocalTime;
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class RendezVous {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateRDV;
-    private Date heureRDV;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "patient_id")
+    @Temporal(TemporalType.TIME)
+    private LocalTime heureRDV;
+
+    @ManyToOne(targetEntity = Patient.class, fetch = FetchType.LAZY)
     private Patient patient;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "medecin_id")
+    @ManyToOne(targetEntity = Medecin.class, fetch = FetchType.LAZY)
     private Medecin medecin;
 
-    @OneToOne
+    @OneToOne(mappedBy = "rendezVous", targetEntity = Consultation.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Consultation consultation;
 }
