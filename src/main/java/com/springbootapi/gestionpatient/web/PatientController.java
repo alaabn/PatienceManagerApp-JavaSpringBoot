5892d3564bce2c4d726c073a5d298418b3a0b997
alaabn <bennasralaa@gmail.com>
2023-09-18 22:22:31 +0100

    Fix Patient Controller

--- src/main/java/com/springbootapi/gestionpatient/web/PatientController.java
+++ src/main/java/com/springbootapi/gestionpatient/web/PatientController.java
@@ -1,51 +1,91 @@
 package com.springbootapi.gestionpatient.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springbootapi.gestionpatient.entities.Patient;
-import com.springbootapi.gestionpatient.service.IPatientService;
+import com.springbootapi.gestionpatient.services.IPatientService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class PatientController {
 
-    @Autowired
-    IPatientService ps;
+    private final IPatientService ps;
+
+    @GetMapping("/patient")
+    public String getAllPatients(Model m,
+            @RequestParam(name = "kw", defaultValue = "") String kw,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+
+        Page<Patient> allPatientPaginated = ps.getallpatient(kw, PageRequest.of(page, size));
+
+        m.addAttribute("data", allPatientPaginated.getContent());
+        m.addAttribute("kw", kw);
+        m.addAttribute("pages", new int[allPatientPaginated.getTotalPages()]);
+        m.addAttribute("current", page);
+
+        return "patient/patientIndex";
+    }
 
     @GetMapping("/addPatient")
-    public String addPatient(Patient p, Model m) {
-        m.addAttribute("patientData", p);
-        return "patientform";
+    public String addPatientForm(Patient p, Model m) {
+
+        m.addAttribute("patient", p);
+        return "patient/patientForm";
+    }
+
+    @PostMapping("/addPatient")
+    public String addPatient(
+            @Valid @ModelAttribute Patient patient,
+            BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "patient/patientForm";
+        }
+
+        ps.addOrUpdatePatient(patient);
+        return "redirect:/patient";
     }
 
     @GetMapping("/updatePatient")
-    public String UpdatePatient(@RequestParam("id") Long id, Model m) {
+    public String UpdatePatientForm(@RequestParam("id") Long id, Model m) {
+
         Patient p = ps.getPatientById(id);
-        m.addAttribute("patientVar", p);
-        return "patientIndex";
+        m.addAttribute("patient", p);
+        return "patient/patientForm";
+    }
+
+    @PostMapping("/updatePatient")
+    public String updatePatient(
+            Model model,
+            @Valid @ModelAttribute Patient patient,
+            BindingResult result) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("patient", patient);
+            return "patient/patientForm";
+        }
+
+        ps.addOrUpdatePatient(patient);
+        return "redirect:/patient";
     }
 
     @GetMapping("/deletePatient")
     public String deletePatient(@RequestParam("id") Long id) {
+
         ps.deletePatient(id);
         return "redirect:/Index";
     }
 
-    @GetMapping("/Patient")
-    public String getAllPatients(Model m,
-            @RequestParam(name = "ps", defaultValue = "") String p,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size) {
-        Page<Patient> pagePatients = ps.getallpatient(p, PageRequest.of(page, size));
-        m.addAttribute("data", pagePatients.getContent());
-        m.addAttribute("p", p);
-        m.addAttribute("pages", new int[pagePatients.getTotalPages()]);
-        m.addAttribute("current", page);
-        return "viewPatient";
-    }
 }
