b7a97cbdadd1ab758e85964fb5af49d66367f127
alaabn <bennasralaa@gmail.com>
2023-09-18 22:14:38 +0100

    Fix OneToMany Relationship

--- src/main/java/com/springbootapi/gestionpatient/entities/Patient.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/Patient.java
@@ -5,16 +5,21 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Patient {
@@ -24,6 +29,6 @@ public class Patient {
     private String nom;
     private String email;
 
-    @OneToMany(mappedBy = "Patient", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<RendezVous> reviews = new ArrayList<RendezVous>();
+    @OneToMany(mappedBy = "patient", targetEntity = RendezVous.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<RendezVous> rendezVous = new ArrayList<RendezVous>();
 }
