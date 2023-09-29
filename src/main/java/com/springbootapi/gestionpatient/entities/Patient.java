b23e2e91b68e988c8f60d90d51162d315af4f490
Enis-Hachicha <hachicha.enis@gmail.com>
2023-09-29 18:50:44 +0100

    Add Validation annotations to Patient Entity

--- src/main/java/com/springbootapi/gestionpatient/entities/Patient.java
+++ src/main/java/com/springbootapi/gestionpatient/entities/Patient.java
@@ -12,6 +12,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -28,10 +31,14 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Pattern(regexp = "[^[A-Z][a-z]+[\\s]{1}]+", message = "le nom complet ne contient que des mots alphabétiques commençant par une majuscule. example: Enis Hachicha")
+    @NotEmpty(message = "veuillez entrer votre nom")
     private String nom;
+    @Email(message = "veuillez entrer un email valide")
+    @NotEmpty(message = "veuillez entrer votre email")
     private String email;
 
-    @JsonManagedReference("pr")
+    @JsonManagedReference("patientRef")
     @OneToMany(mappedBy = "patient", targetEntity = RendezVous.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<RendezVous> rendezVous = new ArrayList<RendezVous>();
 }
